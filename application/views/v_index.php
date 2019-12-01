4acb9067179b45fcce2942e49f203c65c70a0a8f
Boy Kurniawan <boykurniawan123@gmail.com>
2019-12-01 10:31:36 +0700

    perbaikan ke 2

--- application/views/v_index.php
+++ application/views/v_index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Aplikasi Cetak Kartu</title>
+  <title>Aplikasi Reminder</title>
   <base href="<?php echo base_url() ?>">
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
